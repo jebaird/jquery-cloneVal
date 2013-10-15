368844585facc210c258253786f58e46c4b946ab
Jesse Baird <jebaird@gmail.com>
2013-10-15 09:35:30 -0500

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 jquery-cloneVal
 ===============
+@author Jesse Baird
 
-copy :input values to targeted inputs on select events
+Target :inputs in a form and copy their value to select number of targets using event delegation. 
+
+A piratical use case for this plugin would be an e-commerce form with billing and shipping fields containing a checkbox labeled "My billing and shipping addresses are the same" When that's checked copy / clone the values to the shipping address fields.
+
+Take this sample markup into consideration
+
+	<form>
+		<input type="text" name="a">
+		destination<br>
+		<input type="text" name="b">
+		<input type="text" name="c">
+	</form>
+
+If we wanted to copy a's value to b and c, we would setup the plugin like this
+
+	$('form').cloneVal({
+            targets:[
+                [
+                	//copy a input to b and c
+                    "[name=a]",
+                    "[name=b]",
+                    "[name=c]"
+                ]
+            ]
+        });
+
+This sets up event delegation on form in the event namespace "cloneVal"(by default)
+
+If you want to disable this event handlers just call
+
+`$("form").off(".cloneVal")`
---
+++ jquery-cloneVal.js
@@ -0,0 +1,76 @@
+/*
+ * jquery-cloneVal
+ * Target Select inputs in a form and copy their value to select number of targets using event delegation
+ * @author Jesse Baird - jebaird.com
+ *
+ * 
+ * <form>
+ * <input type="text" name="a"><br>
+        destination<br>
+    <input type="text" name="b"><br>
+    <input type="text" name="c"><br>
+ * 
+ * </form>
+ * 
+ * 
+ *  $('form').cloneVal({
+            targets:[
+                [
+                    ".select",
+                    ".target"
+                ],
+                [
+                	//copy a input to b and c
+                    "[name=a]",
+                    "[name=b]",
+                    "[name=c]"
+                ]
+            ]
+        });
+ * 
+ * 
+ */
+(function( $ ) {
+
+	$.fn.cloneVal = function( options ) {
+		var defaults = {
+			eventNS: "cloneVal",
+			events: [ "change", "blur", "keyup" ],
+			targets: [
+			/*
+			 [
+			 ["source","target"]
+			 ]
+			 */
+			]
+		}, 
+		settings = $.extend( {}, defaults, options ), 
+		//create a string like event.ns event2.ns
+		eventString = settings.events.join( "." + settings.eventNS + " " ) + "." + settings.eventNS;
+
+		return this.each( function( ) {
+			
+			var targets = settings.targets, 
+				i = targets.length, 
+				$this = $( this );
+			
+			while( i-- ) {
+				var set = targets[ i ];
+				$this.on( eventString, set[ 0 ], set.slice( 1 ).join( "," ), function( e ) {
+					
+					$this.find( e.data )
+						.val( $( this ).val() )
+						//matche up the values
+						.trigger( e.type )
+				})
+			}
+
+		});
+
+	}
+
+	$( 'form' ).linkInputVals( {
+		targets: [ [ ".select", ".target" ], [ "[name=a]", "[name=b]", "[name=c]" ] ]
+	} );
+
+})( jQuery );
